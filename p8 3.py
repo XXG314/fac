15d558056c30e709d144f0a1bbee9a5e88ee7292
XXG314 <16624684056@163.com>
2025-01-24 15:43:12 +0800

    Add files via upload

--- p8 3.py
+++ p8 3.py
@@ -77,15 +77,9 @@ def par(a):
     a=a>>2
     a=int(a**0.25)+1
     return a
-def par2(m,w,i,n,y,r):
+def par2(m,w,i,n,y,r,r1,u,n1):
     g=1
     d=w
-    v=(i>>1)+r
-    for z in range (1,i):
-        g*=v
-        d+=-1
-        for b in range (d,-1,-1):
-            m[b][z]*=g        
     l=[0]*i
     b=0
     z=i
@@ -94,27 +88,29 @@ def par2(m,w,i,n,y,r):
             l[b]+=g[d]
         z+=-1
         b+=1
+    for n1 in range (n1):
+        for d in range(w):
+            l[d]+=l[d+1]        
     s=fac3(y,r)
+    s*=fac3(r1,u)
     for n in range (n):
         s*=l[0]
         for d in range(w):
             l[d]+=l[d+1]
     return s    
-def par4(i):    
+def par4(i,w):    
     m = [[0 for _ in range(i)] for _ in range(i)]    
-    m[0][1]=-1
-    m[0][3]=1
-    g=i+1
-    s=-4
-    b=-5
-    for f in range (5,g,2):
-        for h in range (f,1,-2):
-            f+=-2
-            m[0][h]*=s
-            m[0][h]+=m[0][f]
-        m[0][1]*=s
-        s+=b
-        b+=-2
+    m[0][1]=1
+    m[0][2]=1
+    for f in range (3,i):
+        d=f-1
+        s=d
+        m[0][f]=m[0][d]
+        for h in range (d,1,-1):
+            s+=-1
+            m[0][h]*=d
+            m[0][h]+=m[0][s]
+        m[0][1]*=d
     return m 
 def par5(l,m,w):
     f=1
@@ -133,23 +129,30 @@ def par5(l,m,w):
     return m
 def par1(i,w):
     l=par3(i,w)
-    m=par4(i)
+    m=par4(i,w)
     return par5(l,m,w)
 def fac3(y,u):
     r=u-y
     a=r
     if r<4000:
         return fac2(y,u)
-    a=a>>2
     a=int(math.sqrt(a))
-    a=a>>1
-    i=a<<1
-    n=i<<2
-    a=n*(i-1)
-    r=u-a
-    w=i-1
+    i=a>>1
+    w=i-1    
+    n=u//w
+    r1=n*w
+    n1=y//w
+    n1+=1
+    r=n1*w
+    if y==0:
+      n1=0
+      r=y
+    if y==1:
+      n1=0
+      r=y
+    n=n-n1
     m=par1(i,w)
-    return par2(m,w,i,n,y,r)
+    return par2(m,w,i,n,y,r,r1,u,n1)
 #以下代码目的为测试上述函数，如果函数值与常规函数计算值一致，则打印成功，多次测试成功可以仅保留上述函数，删除以下测试代码。
 import math
 import time
@@ -157,9 +160,9 @@ import functools
 def main():
      # 记录开始时间
     start_time = time.time()
-    m = 10
+    m=454
     #每次测试请修改i的值，以验证是否成功。
-    i = 1000000
+    i = 65356
     a = fac3(m,i)
             
         # 记录结束时间
